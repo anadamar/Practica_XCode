0b372f96080c9e0d14933732c56082d3d68e5221
Francisco Dasi González <franciscodasigonzalez@MacBook-Air-de-Kiko.local>
2018-11-25 16:52:41 +0100

    Initial Commit

---
+++ Six of Crows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Six of Crows
+//
+//  Created by Francisco Dasi González on 25/11/2018.
+//  Copyright © 2018 AnaDasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Six of CrowsTests/Six_of_CrowsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Six_of_CrowsTests.swift
+//  Six of CrowsTests
+//
+//  Created by Francisco Dasi González on 25/11/2018.
+//  Copyright © 2018 AnaDasi. All rights reserved.
+//
+
+import XCTest
+@testable import Six_of_Crows
+
+class Six_of_CrowsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
